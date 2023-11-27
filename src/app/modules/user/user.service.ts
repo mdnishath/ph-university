2aaf2ed800a77b0cc5fbafe546411ff5a82d56e4
mdnishath <wpprodeveloper@gmail.com>
2023-11-27 01:05:58 -0600

    user, student => model,interface and services added

---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,9 @@
+const createStudent = () => {};
+const createAdmin = () => {};
+const createFaculty = () => {};
+
+export const UserServices = {
+  createStudent,
+  createAdmin,
+  createFaculty,
+};
